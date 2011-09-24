64e75ff0f65945d35b1d931cb8460f1d4f0b17b1
Jan Krüger <jan.krueger@gmail.com>
2011-09-24 14:19:05 +0200

    added tests for all basic parser functions

--- gen/src/main/clojure/com/contentjon/gen/core.clj
+++ gen/src/main/clojure/com/contentjon/gen/core.clj
@@ -4,11 +4,42 @@
   (:refer-clojure :exclude [+ * not or])
   (:use [clojure.contrib.monads]))
 
+(defprotocol AsParser
+  "this section defines what can be turned into a parser function
+    and how this is done"
+  (as-parser [_]))
+
+(defn parser-t [m]
+  (monad [m-result (with-monad m m-result)
+          m-bind   (with-monad m
+                     (fn m-bind-parser-t [p f]
+                       (m-bind (as-parser p) f)))
+          m-zero   (with-monad m m-zero)
+          m-plus   (with-monad m
+                     (fn m-plus-parser-t [& ps]
+                       (apply m-plus (map as-parser ps))))]))
+
 (def #^{:doc "Defines the parser monad. Currently this is (state-t maybe-m).
               This means that a parser returns either nil if it fails or
               a vector containing the parser return value and the remaining
               input that was not matched"}
-     parser-m (state-t maybe-m))
+  parser-m
+  (parser-t (state-t maybe-m)))
+
+;;; macro for defining a parser
+
+(defmacro parser
+  "Creates a new monadic parser with the passed in bindings and body."
+  ([bindings body]
+     `(domonad parser-m ~bindings ~body)))
+
+(defmacro defparser
+  "Defines a new monadic parser with the passed in name,
+   bindings and body."
+  ([name bindings body]
+     `(def ~name (parser ~bindings ~body))))
+
+;;; helpers for calling parsers
 
 (def #^{:doc "Takes the result of a parser and checks if it failed"}
   parser-fail? nil?)
@@ -25,63 +56,18 @@
        (empty? (second in))))
 
 (defn parse
-  "Uses a parser to parse the input and returns the result of the parsing process"
+  "Uses a parser to parse the input and returns the result of the parsing
+   process if the parser consumes all input"
+  [rule in]
+  (when-let [parsed (rule in)]
+    (when (parser-finished? parsed)
+      (first parsed))))
+
+(defn parse-partial
+  "Uses a parser to parse the input and returns a vector of parser
+   result and unconsumed input"
   [rule in]
   (rule in))
-
-(defprotocol AsParser
-  "this section defines what can be turned into a parser function
-    and how this is done"
-  (as-parser [_]))
-
-;;; helpers for defining or running parsers
-
-(defn- skip-transform-bindings [skip-parser-sym bindings]
-  (let [skip-binding `[_# (* ~skip-parser-sym)]
-        bindings (->> bindings
-                      (partition 2)
-                      (interpose skip-binding)
-                      (apply concat))]
-    (concat skip-binding bindings skip-binding)))
-
-(defn- bindings->parser [bindings]
-  (->> bindings
-       (partition 2)
-       (map (fn [[s p]]
-              (if (some #{s} [:when :let])
-                [s p]
-                (vector s `(as-parser ~p)))))
-       (apply concat)))
-
-(defmacro parser
-  "Creates a new monadic parser with the passed in bindings and body.
-   It optionally accepts :skip skip-parser as first arguments, in which
-   case skip-parser is run before and after each subparsers. This is useful
-   for dealing with whitespace."
-  ([bindings body]
-     `(domonad parser-m
-               [~@(bindings->parser bindings)]
-               ~body))
-  ([skip-keyword skip bindings body]
-     (when-not (= skip-keyword :skip)
-       (throw (IllegalArgumentException. "use :skip")))
-     (let [skip-parser-sym (gensym "skip-parser")
-           bindings (skip-transform-bindings skip-parser-sym bindings)]
-       `(let [~skip-parser-sym ~skip]
-          (parser ~bindings
-                  ~body)))))
-
-(defmacro defparser
-  "Defines a new monadic parser with the passed in name, bindings and body.
-   Can deal with :skip skip-parser like the parser macro."
-  ([name bindings body]
-     `(def ~name (parser ~bindings
-                         ~body)))
-  ([name skip-keyword skip bindings body]
-     `(def ~name (parser ~skip-keyword
-                         -skip
-                         ~bindings
-                         ~body))))
 
 ;;; public matching functions
 
@@ -107,12 +93,11 @@
                (predicate (first in)))
       [(first in) (rest in)])))
 
-(defn not
+(defn !
   "A parser that fails when p succeeds and succeeds when p fails"
   [p]
   (fn [in]
-    (if (p in)
-      nil
+    (when-not ((as-parser p) in)
       [nil in])))
 
 (def #^{:doc "Returns a parser that matches one of multiple alternatives"
@@ -135,9 +120,9 @@
   "Returns a parser that applies a subparser 1 or more times to it's input"
   [rule]
   (parser [first rule
-           rest  (* rule)]
-
-    (concat [first] rest)))
+           rest  (if first (* rule) (lambda))]
+    (when first
+      (cons first rest))))
 
 (defn times
   "A parser that applies p between min and max times.
@@ -151,23 +136,12 @@
                  (lambda)
                  (let [next (parser [f p
                                      r (next-parser (inc n))]
-                              (cons f r))]
+                              (if f
+                                (cons f r)))]
                    (if (< n min)
                      next
                      (? next)))))]
        (next-parser 0))))
-
-;;; helper functions that can be used to verify the state of a parser but don't consume
-;;; any input
-
-(defn assert-state
-  "Returns a parser that matches nothing. It returns a nil value and its input as its state.
-   Before it does this it applies a predicate to its input state and lets the parser fail
-   if it doesn't match"
-  [predicate]
-  (parser [state (fetch-state)
-	   _     #(if (predicate state) [% state] nil)]
-	  nil))
 
 ;;; some useful parsers
 
@@ -188,34 +162,38 @@
   ([before after]
      #(surround % before after)))
 
-(defn log
-  "Wraps a parser and logs its input"
-  [msg rule]
-  (fn [in]
-    (println msg in)
-    (rule in)))
+;;; types extended to be parsers
 
-;;; parser coercsions
+(defn assert-state
+  "Returns a parser that matches nothing. It returns a nil value and its input as its state.
+   Before it does this it applies a predicate to its input state and lets the parser fail
+   if it doesn't match"
+  [predicate]
+  (parser [state (fetch-state)
+	   _     #(if (predicate state) [% state] nil)]
+	  nil))
 
 (defn- without-match [match in]
   (.substring in (.length match) (.length in)))
 
 (defn descend [rule]
-  (parser [stream (fetch-state) :when (clojure.core/not (empty? stream))
-           :let  [f (first stream)]
+  (parser [stream (fetch-state)
+           :when  (clojure.core/not (empty? stream))
+           :let   [f (first stream)]
+           :when  (clojure.core/or (string? f) (sequential? f))
            _      (set-state (if (string? f)
                                f
                                (seq (first stream))))
            result rule
            _      (assert-state empty?)
            _      (set-state (rest stream))]
-          result))
+    result))
 
 (defn maybe-descend [rule]
   (fn [in]
     (if (sequential? in)
-      (parse (descend rule) in)
-      (parse rule in))))
+      ((descend rule) in)
+      (rule in))))
 
 (extend-protocol AsParser
   java.lang.Character
@@ -229,20 +207,21 @@
   (as-parser [this]
     (maybe-descend
      (fn [in]
-        (when (.startsWith in this)
-          [this (without-match this in)]))))
+       (when (and  (string? in)
+                   (.startsWith in this))
+         [this (without-match this in)]))))
   java.util.regex.Pattern
   (as-parser [this]
     (maybe-descend
      (fn [in]
        (let [pat (java.util.regex.Pattern/compile (str "^" this ""))
              m   (.matcher pat in)]
-         (when (.find m)
+         (when (and (string? in) (.find m))
            (let [match (.group m)]
              [match (without-match match in)]))))))
   clojure.lang.IPersistentVector
   (as-parser [this]
-    (let [rule (* (apply or (map as-parser this)))]
+    (let [rule (* (apply or this))]
       (descend rule)))
   clojure.lang.IPersistentMap
   (as-parser [this]
