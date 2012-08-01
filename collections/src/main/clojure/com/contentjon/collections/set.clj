c633ef709602b8f923d0f7d0e6f47994da03bef9
Benjamin Teuber <bsteuber@gmail.com>
2012-08-01 11:17:38 +0200

    set/disj

--- collections/src/main/clojure/com/contentjon/collections/set.clj
+++ collections/src/main/clojure/com/contentjon/collections/set.clj
@@ -1,5 +1,5 @@
 (ns com.contentjon.collections.set
-  (:refer-clojure :exclude [conj map])
+  (:refer-clojure :exclude [conj disj map])
   (:require [clojure.core :as clj]))
 
 (defn as-set
@@ -12,6 +12,10 @@
 (def conj
   "Like clojure's conj, but always returns a set"
   (comp as-set clj/conj))
+
+(def disj
+  "Like clojure's disj, but always returns a set"
+  (comp clj/disj as-set))
 
 (def map
   "Like clojure's map, but always returns a set"
