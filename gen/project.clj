08b5b9703aaec220aa8a9c587216ba1cd9083a25
Benjamin Teuber <bsteuber@gmail.com>
2013-01-22 11:09:38 +0100

    fix dependency to fn in gen

--- gen/project.clj
+++ gen/project.clj
@@ -1,6 +1,6 @@
 (defproject com.contentjon.tools/gen "1.3-SNAPSHOT"
   :dependencies [[org.clojure/clojure     "1.3.0"]
                  [org.clojure/algo.monads "0.1.0"]
-                 [com.contentjon.tools/fn "1.2-SNAPSHOT"]]
+                 [com.contentjon.tools/fn "1.3-SNAPSHOT"]]
   :source-paths ["src/main/clojure"]
   :test-paths   ["src/test/clojure"])
